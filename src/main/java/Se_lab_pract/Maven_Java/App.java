13f37b0604ebe02f752fc8e022ccf0c763855fa0
Y.Rishitha <rishitha@gmail.com>
2025-11-29 20:43:50 +0530

    first commit

---
+++ src/main/java/Se_lab_pract/Maven_Java/App.java
@@ -0,0 +1,10 @@
+package Se_lab_pract.Maven_Java;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!Rishitha");
+    }
+}
---
+++ src/test/java/Se_lab_pract/Maven_Java/AppTest.java
@@ -0,0 +1,19 @@
+package Se_lab_pract.Maven_Java;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
